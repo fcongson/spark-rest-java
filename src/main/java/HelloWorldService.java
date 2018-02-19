6a7ecf360afc220c6a16f3f16b0e9613a90c19e5
Frank Congson <fcongson@gmail.com>
2018-02-20 12:02:19 +1300

    Format code

--- src/main/java/HelloWorldService.java
+++ src/main/java/HelloWorldService.java
@@ -3,10 +3,10 @@ import static spark.Spark.get;
 public class HelloWorldService {
     public static void main(String[] args) {
 
-        get("/hello", (req, res)->"Hello, world");
+        get("/hello", (req, res) -> "Hello, world");
 
-        get("/hello/:name", (req,res)->{
-            return "Hello, "+ req.params(":name");
+        get("/hello/:name", (req, res) -> {
+            return "Hello, " + req.params(":name");
         });
     }
 }
