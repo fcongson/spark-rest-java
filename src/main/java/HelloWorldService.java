a2ef4a1666bd64add27eacf721d9aab93e4dfaa7
Frank Congson <fcongson@gmail.com>
2018-02-19 17:45:40 +1300

    Add hello endpoints

---
+++ src/main/java/HelloWorldService.java
@@ -0,0 +1,12 @@
+import static spark.Spark.get;
+
+public class HelloWorldService {
+    public static void main(String[] args) {
+
+        get("/hello", (req, res)->"Hello, world");
+
+        get("/hello/:name", (req,res)->{
+            return "Hello, "+ req.params(":name");
+        });
+    }
+}
