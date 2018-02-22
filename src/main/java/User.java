84bde31a061267d5b66b5e953d7a292a6aa76fc7
Frank Congson <fcongson@gmail.com>
2018-02-22 17:58:12 +1300

    Add lombok

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,39 +1,10 @@
+import lombok.Data;
+
+@Data
 public class User {
 
     private String id;
     private String email;
     private String firstName;
     private String lastName;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
